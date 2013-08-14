154d510f9a323393cee39a2e2c27fb343b5ab07e
nmred <nmred_2008@126.com>
2013-08-14 22:41:53 +0800

    tmp

---
+++ tests/mock/controller/action/helper/sw_abstract_mock.class.php
@@ -0,0 +1,35 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4 foldmethod=marker: */
+// +---------------------------------------------------------------------------
+// | SWAN [ $_SWANBR_SLOGAN_$ ]
+// +---------------------------------------------------------------------------
+// | Copyright $_SWANBR_COPYRIGHT_$
+// +---------------------------------------------------------------------------
+// | Version  $_SWANBR_VERSION_$
+// +---------------------------------------------------------------------------
+// | Licensed ( $_SWANBR_LICENSED_URL_$ )
+// +---------------------------------------------------------------------------
+// | $_SWANBR_WEB_DOMAIN_$
+// +---------------------------------------------------------------------------
+ 
+namespace mock\controller\action\helper;
+use lib\controller\action\helper\sw_abstract;
+
+/**
++------------------------------------------------------------------------------
+* sw_abstract_mock 
++------------------------------------------------------------------------------
+* 
+* @uses sw
+* @uses _abstract
+* @package 
+* @version $_SWANBR_VERSION_$
+* @copyright $_SWANBR_COPYRIGHT_$
+* @author $_SWANBR_AUTHOR_$ 
++------------------------------------------------------------------------------
+*/
+class sw_abstract_mock extends sw_abstract
+{
+	// {{{ functios
+	// }}}
+}
